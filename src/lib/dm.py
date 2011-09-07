3777ebbf15207690943578cc3d55d06644c93566
matu <matias.romeo@gmail.com>
2011-09-06 21:09:08 -0300

    DM Payment

--- src/lib/dm.py
+++ src/lib/dm.py
@@ -10,8 +10,8 @@ import urllib
 IPN_URL     = 'http://argentina.dineromail.com/vender/ConsultaPago.asp'
 #IPN_URL     = 'http://localhost:8080/img/ipn.xml'
 IPN_PARAMS  = {'Email'    :'ptutino@gmail.com',
-               'Acount'   :'09813581',
-               'Pin'      :'URJYRLU2PP',
+               'Acount'   :'22676371',
+               'Pin'      :'50XBXYROC5',
                'XML'      :'1'}
 
 def ipn_download(date_from, date_to):
@@ -19,7 +19,7 @@ def ipn_download(date_from, date_to):
   IPN_PARAMS['EndDate'  ] = date_to.strftime('%Y%m%d')
 
   url    = IPN_URL + '?' + urllib.urlencode(IPN_PARAMS)
-  logging.error('Voy a pedir %s' % url)
+  logging.info('Voy a pedir %s' % url)
   
   result = urllib2.urlopen(url).read()
   return result
