37427dbf138c1de412a42c3b3e50fe1f395d9b5d
Emilio Maull <emiliomaull@gmail.com>
2011-09-20 17:11:57 -0300

    ayuda

--- src/apps/backend/help.py
+++ src/apps/backend/help.py
@@ -9,4 +9,4 @@ from utils import need_auth, BackendHandler
 class Index(BackendHandler):
   @need_auth(code=500)
   def get(self, **kwargs):
-    return self.render_response('backend/help.html')
+    return self.render_response('backend/help.html',mnutop='ayuda')
