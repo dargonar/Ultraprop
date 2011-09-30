7b27b92a336682106697dec59be7414c60d6f0ea
Pablo <ptutino@gmail.com>
2011-09-30 15:31:15 -0300

    terminos y condiciones

--- src/lib/models.py
+++ src/lib/models.py
@@ -41,7 +41,10 @@ class RealEstate(db.Model):
   
   @classmethod
   def new(cls):
-    return RealEstate(status=RealEstate._TRIAL, managed_domain=0)
+    rs = RealEstate(status=RealEstate._TRIAL, managed_domain=0)
+    re.tpl_title  = u'Hacemos más fácil, rápida y segura su operación inmobiliaria'
+    re.tpl_text   = u'Nuestra inmobiliaria se ha convertido en una empresa moderna y dinámica. Hoy cuenta con los más modernos sistemas de comercialización, con los recursos humanos y con la tecnología necesarios para realizar con éxito sus negocios inmobiliarios.'
+  return rs
     
   logo                = blobstore.BlobReferenceProperty() #--Borrar--
   logo_url            = db.StringProperty(indexed=False)
