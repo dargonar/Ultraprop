8bef0d202c9866fd78566b571c1cd691c0f95d2d
Matu <matias.romeo@gmail.com>
2012-01-05 19:56:55 -0300

    agregado url para invocar importador

--- src/apps/backend/urls.py
+++ src/apps/backend/urls.py
@@ -16,6 +16,7 @@ def get_rules():
       Route('/tsk/run_ipn/<account>'    , name='billing/payment/download'     , handler='apps.backend.payment.Download'),
       Route('/tsk/run_invoicer'         , name='billing/payment/run_invoicer' , handler='apps.backend.payment.RunInvoicer'),
       Route('/tsk/update_ipn/<account>' , name='billing/payment/update_ipn'   , handler='apps.backend.payment.UpdateIPN'),
+      Route('/tsk/inmob/<inmoid>'       , name='backend/import/inmob'         , handler='apps.backend.import.InmoBusqueda'),
       
       # Todas las rutas de billing
       PathPrefixRoute('/billing', [
